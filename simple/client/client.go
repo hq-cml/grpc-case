8d31a42cf41e1b2595634a3c6e5d6c3db11252eb
hq <hq_cml@163.com>
2024-06-30 17:57:03 +0800

    simple server&client

---
+++ simple/client/client.go
@@ -0,0 +1,37 @@
+/**
+ * 简单客户端
+ */
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"grpc-case/simple/pb"
+)
+
+func main() {
+	// 创建链接，此处警用安全传输，没有加密和验证
+	//conn, err := grpc.Dial("127.0.0.1: 9090", grpc.WithTransportCredentials(insecure.NewCredentials()))   # 废弃
+	//conn, err := grpc.DialContext(context.Background(), "127.0.0.1: 9090", grpc.WithTransportCredentials(insecure.NewCredentials())) # 废弃
+	conn, err := grpc.NewClient("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// 建立连接并创建客户端
+	client := pb.NewHelloServiceClient(conn)
+
+	// 执行RPC调用
+	rsp, err := client.SayHello(context.Background(), &pb.HelloRequest{
+		Name: "far",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// 返回结果
+	fmt.Println("Recv: ", rsp.Message)
+}
---
+++ simple/server/server.go
@@ -0,0 +1,54 @@
+/**
+ * 简单服务端
+ */
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-case/simple/pb"
+	"net"
+	"sync"
+)
+
+// 业务自己的Server，实现各个服务端方法
+type MyServer struct {
+	pb.UnimplementedHelloServiceServer //首先包装一个UnimplementedServer，使自身成为HelloServiceServer接口的实现
+}
+
+// 实现业务代码
+func (m *MyServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	fmt.Println("Recv Request:", req.Name)
+	return &pb.HelloReply{
+		Message: "Hello " + req.Name,
+	}, nil
+}
+
+// 服务启动起来
+func main() {
+	// 创建监听端口
+	listener, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		panic(err)
+	}
+
+	// 创建grpc服务
+	grpcServer := grpc.NewServer()
+
+	// 在grpc服务中，注册业务自己的服务
+	pb.RegisterHelloServiceServer(grpcServer, &MyServer{})
+
+	// 启动grpc服务
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		fmt.Println("Server start!")
+		defer wg.Done()
+		err = grpcServer.Serve(listener)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	wg.Wait()
+}
