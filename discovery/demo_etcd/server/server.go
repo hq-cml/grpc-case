83372cc64a72faf689bbc0d0fa04e0dfd48a0120
hq <hq_cml@163.com>
2024-07-09 07:44:12 +0800

    服务发现，基于Etcd

--- discovery/demo_etcd/server/server.go
+++ discovery/demo_etcd/server/server.go
@@ -1,7 +1,6 @@
 /**
  * 服务发现服务端，支持指定port
- * 在简单服务发现的例子中，服务端是比较简单的，只是简单的增加了一个port参数
- * 服务发现的文章，都在客户端实现（实际场景上，结合ETCD，则需要在服务端增加一些注册的逻辑）
+ * 在Etcd服务发现的例子中，服务端增加了注册Etcd的逻辑
  */
 package main
 
@@ -19,6 +18,12 @@ import (
 )
 
 var portStr *string = flag.String("p", "9090", "port")
+var nameStr *string = flag.String("n", "myservice", "service name")
+
+const (
+	EtcdAddr    = "127.0.0.1:2379"
+	EtcdTimeout = 1
+)
 
 // 业务自己的Server，实现各个服务端方法
 type MyServer struct {
@@ -50,61 +55,78 @@ func main() {
 
 	// 在grpc服务中，注册业务自己的服务（也就是将自己的Server对象与grpc服务绑定）
 	pb.RegisterHelloServiceServer(grpcServer, &MyServer{})
-	etcdCli, err := etcdCLientv3.New(etcdCLientv3.Config{
-		Endpoints: []string{"127.0.0.1:2379"},
-		//DialTimeout: e.dialTimeout, TODO
-	})
-	if err != nil {
-		panic(err)
-	}
-	err = Register(context.Background(), etcdCli, "A", addr)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("X----------------------")
+
 	// 启动grpc服务
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
-		fmt.Printf("Server Start! Port:%v\n", *portStr)
+		fmt.Printf("Server Start! Name:%v, Port:%v\n", *nameStr, *portStr)
 		defer wg.Done()
 		err = grpcServer.Serve(listener)
 		if err != nil {
 			panic(err)
 		}
 	}()
+
+	// 服务注册到Etcd
+	ctx, cancel := context.WithCancel(context.Background())
+	err = registerToEtcd(ctx, *nameStr, addr)
+	if err != nil {
+		panic(err)
+	}
+
+	// 等待
 	wg.Wait()
+
+	// 关闭etcd相关操作
+	cancel()
 }
 
-func Register(ctx context.Context, client *etcdCLientv3.Client, service, addr string) error {
-	resp, err := client.Grant(ctx, 1)
+// 注册到Etcd
+func registerToEtcd(ctx context.Context, service, addr string) error {
+	// 创建客户端
+	etcdCli, err := etcdCLientv3.New(etcdCLientv3.Config{
+		Endpoints:   []string{EtcdAddr},
+		DialTimeout: EtcdTimeout * time.Second,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// 创建租约
+	resp, err := etcdCli.Grant(ctx, 1)
 	if err != nil {
 		return fmt.Errorf("Grant Err:%v", err)
 	}
 	fmt.Println("Grant-------", resp.ID)
 
+	// 注册
 	key := strings.Join([]string{service, addr}, "/")
-	_, err = client.Put(ctx, key, addr, etcdCLientv3.WithLease(resp.ID))
+	_, err = etcdCli.Put(ctx, key, addr, etcdCLientv3.WithLease(resp.ID))
 	if err != nil {
 		return fmt.Errorf("Etcd Put Err:%v", err)
 	}
 	fmt.Println("Put-------", key, "=>", addr)
-	// respCh 需要消耗, 不然会有 warning
-	respCh, err := client.KeepAlive(ctx, resp.ID)
+
+	// 租约保活
+	respCh, err := etcdCli.KeepAlive(ctx, resp.ID)
 	if err != nil {
 		return fmt.Errorf("Etcd Keep Alive Err:%v", err)
 	}
 
+	// 异步监听保活的结果
 	go func() {
 		for {
 			select {
 			case <-ctx.Done():
-				//return nil
+				fmt.Println("实例退出")
+				return
 			case v, ok := <-respCh:
 				if v == nil {
 					fmt.Println("租约失效", ok)
+					return
 				} else {
-					fmt.Println(time.Now().Format(time.DateTime), "租约成功", v, ok)
+					// fmt.Println(time.Now().Format(time.DateTime), "租约成功", v, ok)
 				}
 			}
 		}
@@ -113,7 +135,7 @@ func Register(ctx context.Context, client *etcdCLientv3.Client, service, addr st
 	// 测试租约回收
 	//go func() {
 	//	time.Sleep(10 * time.Second)
-	//	_, err = client.Revoke(ctx, resp.ID) // 撤销租约
+	//	_, err = etcdCli.Revoke(ctx, resp.ID) // 撤销租约
 	//	if err != nil {
 	//		panic(err)
 	//	}
